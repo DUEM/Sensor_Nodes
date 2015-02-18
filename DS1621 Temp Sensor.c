f5cd99eff46a19b51343c228224bc2402956a479
goredon0 <oliver.groeling@gmail.com>
2015-02-18 15:08:21 +0000

    Update DS1621 Temp Sensor.c

--- DS1621 Temp Sensor.c
+++ DS1621 Temp Sensor.c
@@ -9,10 +9,10 @@ void setup()
 
     Wire.begin();
     Wire.beginTransmission(DEV_ID);             // connect to DS1621 (#0)
-    Wire.send(0xAC);                            // Access Config
-    Wire.send(0x02);                            // set for continuous conversion
+    Wire.write(0xAC);                            // Access Config
+    Wire.write(0x02);                            // set for continuous conversion
     Wire.beginTransmission(DEV_ID);             // restart
-    Wire.send(0xEE);                            // start conversions
+    Wire.write(0xEE);                            // start conversions
     Wire.endTransmission();
 }
 
@@ -26,12 +26,12 @@ void loop()
     delay(1000);                                // give time for measurement
 
     Wire.beginTransmission(DEV_ID);
-    Wire.send(0xAA);                            // read temperature command
+    Wire.write(0xAA);                            // read temperature command
     Wire.endTransmission();
     Wire.requestFrom(DEV_ID, 2);		// request two bytes from DS1621 (0.5 deg. resolution)
 
-    firstByte = Wire.receive();		        // get first byte
-    secondByte = Wire.receive();		// get second byte
+    firstByte = Wire.read();		        // get first byte
+    secondByte = Wire.read();		// get second byte
 
     temp = firstByte;
 
@@ -41,6 +41,3 @@ void loop()
     Serial.println(temp);
 
 }
-
-
-
