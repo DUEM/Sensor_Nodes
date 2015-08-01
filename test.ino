b676ffeba4dc0ea228ae5c9e2bb080552437953a
EndingCredits <awwoof@hotmail.com>
2015-08-01 13:37:46 +0100

    Update test.ino

--- test.ino
+++ test.ino
@@ -8,12 +8,24 @@
 ////////////////////////////////////////////////
 
 #define DEVICE_NODE_ID          MAIN_CONTROLLER_ID
-#define DEVICE_RESET_ID         DEVICE_NODE_ID | 0x500
+#define DEVICE_RESET_ID         DEVICE_NODE_ID + SENSOR_NODE_ID_RANGE
 
 #define DEVICE_NODE_TYPE        MAIN_CONTROLLER
 
-#define SHORT_TIMER_PERIOD      100     //millisecs
+#define SHORT_TIMER_PERIOD      50     //millisecs
 #define LONG_TIMER_PERIOD       1000   //millisecs
+
+////////////////////////////////////////////////
+// Pins
+////////////////////////////////////////////////
+
+#define CAN_HW_ENABLE_PIN      10
+#define HEARTBEAT_LED_PIN      7
+
+#define CRUISE_CONTROL_PIN     A5
+#define ACCELERATOR_PIN        4
+#define FORWARDS_PIN           3
+#define REVERSE_PIN            2
 
 ////////////////////////////////////////////////
 // Global Variables
@@ -21,10 +33,28 @@
 
 bool strobea=0; //var for heartbeat
 
+// Outputs for Motor
+float motor_set_speed = 0.0f;
+float motor_set_current = 0.0f;
+float bus_set_current = 0.0f;
+
+// Inputs from driver
+float driver_set_speed = 0.0f; //Input from potentiometer
+bool accel_pedal_pressed = 0;
+bool forwards_switch_pressed = 0;
+bool reverse_switch_pressed = 0;
+
+bool ignition_set = 0;
+bool reverse_set = 0;
+
 long short_timer_last = 0;
 long short_timer_period = SHORT_TIMER_PERIOD;
 long long_timer_last = 0;
 long long_timer_period = LONG_TIMER_PERIOD;
+
+EightByteData eight_byte_data;
+
+MCP_CAN CAN(CAN_HW_ENABLE_PIN);
 
 ////////////////////////////////////////////////
 
@@ -65,8 +95,8 @@ START_INIT:
     //Filters for RXB1
     CAN.init_Filt(2, 0, CAN_DEFAULT_FILTER);
     CAN.init_Filt(3, 0, CAN_DEFAULT_FILTER);
-    CAN.init_Filt(4, 0, CAN_DEFAULT_FILTER);
-    CAN.init_Filt(5, 0, CAN_DEFAULT_FILTER);
+    CAN.init_Filt(4, 0, MOTOR_CONTROL_ID_BASE);
+    CAN.init_Filt(5, 0, MOTOR_CONTROL_ID_BASE);
     
     CAN.enableBufferPins();
     
@@ -80,14 +110,19 @@ START_INIT:
     duem_send_message(msg_out);
     
     //enable LED for heartbeat
-    pinMode(7, OUTPUT);
+    pinMode(HEARTBEAT_LED_PIN, OUTPUT);
+    
+    //enable switch inputs
+    pinMode(ACCELERATOR_PIN, INPUT_PULLUP);
+    pinMode(FORWARDS_PIN, INPUT_PULLUP);
+    pinMode(REVERSE_PIN, INPUT_PULLUP);    
 
 }
 
 ////////////////////////////////////////////////
 
 void loop()
-{
+{   
     
     ////////////////////////////////////////////////
     // SHORT LOOP
@@ -97,6 +132,33 @@ void loop()
     long milliseconds = millis();
     if ( (milliseconds >= short_timer_last + short_timer_period) || (milliseconds < short_timer_last) ) {
         //second condition just in case timer ticks over
+        
+        //INPUTS WILL BE LOW WHEN BUTTON IS PRESSED
+        int input_a = digitalRead(ACCELERATOR_PIN);
+        int input_b = digitalRead(FORWARDS_PIN);
+        int input_c = digitalRead(REVERSE_PIN);
+        
+        if (input_b == LOW) {
+          forwards_switch_pressed = 1; reverse_set = 0; ignition_set = 1;
+        } else if (input_c == LOW) {
+          reverse_switch_pressed = 1; reverse_set = 1; ignition_set = 1;
+        } else { forwards_switch_pressed = 0; reverse_switch_pressed = 0; ignition_set = 0; reverse_set = 0; }
+        
+        if (input_a = LOW) {
+          accel_pedal_pressed = 1;
+          motor_set_current = 0.75;
+        } else {
+          accel_pedal_pressed = 0;
+          motor_set_current = 0.0;          
+        }
+        
+        if (ignition_set) {
+          // Send Velocity + Current Message
+          eight_byte_data.f[0] = motor_set_speed; //low float
+          eight_byte_data.f[1] = motor_set_current; //high float
+          
+          CAN.sendMsgBuf(DRIVER_CONTROL_ID_BASE+1, 0, 8, eight_byte_data.c);
+        }
 
         //reset last counter
         short_timer_last = millis();
@@ -111,7 +173,15 @@ void loop()
         
         //Update Heartbeat
         strobea = 1-strobea;
-        digitalWrite(7, strobea);
+        digitalWrite(HEARTBEAT_LED_PIN, strobea);
+        
+        /////////////////
+        // Send Bus Current Message
+        eight_byte_data.f[0] = 0.0f; //low float
+        eight_byte_data.f[1] = bus_set_current; //high float
+        
+        CAN.sendMsgBuf(DRIVER_CONTROL_ID_BASE+2, 0, 8, eight_byte_data.c);
+        /////////////////
         
         //reset last counter
         long_timer_last = millis();
@@ -125,7 +195,14 @@ void loop()
     if(CAN_MSGAVAIL == CAN.checkReceive())            // check if data coming
     {
         CAN.readMsgBufID(&message_id, &message_len, message_buf);
-        DUEMCANMessage msg = duem_rcv_message(message_id, message_len, message_buf);
+        
+        if ((message_id >= SENSOR_NODE_ID_BASE) && (message_id < SENSOR_NODE_ID_BASE+SENSOR_NODE_ID_RANGE)) {
+          DUEMCANMessage msg = duem_rcv_message(message_id, message_len, message_buf);
+          
+          
+        } else if ((message_id >= MOTOR_CONTROL_ID_BASE) && (message_id < MOTOR_CONTROL_ID_BASE+MOTOR_CONTROL_ID_RANGE)) {
+          //Handle Motor Controller Messages
+        }
         
         // Print message to serial for debugging
         Serial.print(message_id); Serial.print(":\t");
